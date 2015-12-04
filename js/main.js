650b658ec4e7b58f840175f07175d0bf6227e7d9
Steve Swanson <steve.e.swanson@gmail.com>
2015-12-04 13:39:31 -0600

    =Cleanup. More consistent use of self, cookie helper

--- js/main.js
+++ js/main.js
@@ -39,11 +39,11 @@ define([
     var bookmrksView = new BookmrksView({ el: $('#bookmrks') });
     self.makeZones();
   }
-  BookMrks.prototype.makeZones = function(){ console.log('goz')
+  BookMrks.prototype.makeZones = function(){
     var self=this;
 
-    if(self.zones === undefined){
-      EventsBB.trigger('newZone', { init: true });
+    if(!_.size(self.zones)){
+      EventsBB.trigger('newZone');
     } else {
       var cookieData = self.c.get();
 
@@ -56,26 +56,21 @@ define([
       }
     }    
   }
-  BookMrks.prototype.newZone = function(payload){
+  BookMrks.prototype.newZone = function(){
     var self=this;
     var id = "zone"+Math.floor(Math.random() * 10000);
     var data = { title: "New", id:id };
     var zoneView = new ZoneView({ model: data });
-    var cookieData;
-
-    if(payload && payload.init){
-      cookieData = {};
-    } else {
-      cookieData = self.c.get();
-    }
+    var cookieData = self.c.get();
     
     cookieData[id] = data;
     self.c.set(cookieData);
   }
   BookMrks.prototype.updateHeader = function(payload){
+    var self = this;
     var title = payload.title;
     var id = payload.id;
-    var cookieData = JSON.parse( Cookies.get('zones') );
+    var cookieData = self.c.get();
 
     cookieData[id].title = title;
 
@@ -83,7 +78,8 @@ define([
   }  
   BookMrks.prototype.c = {
     get: function(){
-      return JSON.parse( Cookies.get('zones') );
+      var zones = Cookies.get('zones');
+      return  zones === undefined ? {} : JSON.parse( zones );
     },
     set: function(data){
       Cookies.set('zones', JSON.stringify(data) );
@@ -91,66 +87,5 @@ define([
   }
 
   var bookMrks = new BookMrks();
-
-  // var Zones = Cookies.get('zones');
-  // // Creates the overall container
-  // var bookmrksView = new BookmrksView({ el: $('#bookmrks') });
-
-
-  // EventsBB.on('newZone', function(payload){
-  //   newZone(payload);
-  // });
-
-
-  // // Beginnings of loading data from the stored cookie
-  // if(Zones === undefined){
-  //   EventsBB.trigger('newZone', { init: true });
-  // } else {
-  //   var cookieData = JSON.parse( Zones );
-
-  //   for(var zone in cookieData ) {
-  //     var data = {
-  //       title: cookieData[zone].title,
-  //       id: cookieData[zone].id
-  //     }
-  //     var zoneView = new ZoneView({ model: data });
-  //   }
-  // }
-
-  // EventsBB.on('update', function(payload){
-  //   updateHeader(payload);
-  // });
-
-
-
-
-  // function newZone(payload){
-  //   var id = "zone"+Math.floor(Math.random() * 10000);
-  //   var data = { title: "New", id:id };
-  //   var zoneView = new ZoneView({ model: data });
-  //   var cookieData;
-
-  //   if(payload && payload.init){
-  //     cookieData = {};
-  //   } else {
-  //     cookieData = JSON.parse( Cookies.get('zones') );
-  //   }
-    
-  //   cookieData[id] = data;
-  //   Cookies.set('zones', JSON.stringify(cookieData) );
-  // }
-
-
-  // function updateHeader(payload){
-  //   var title = payload.title;
-  //   var id = payload.id;
-  //   var cookieData = JSON.parse( Cookies.get('zones') );
-
-  //   cookieData[id].title = title;
-
-  //   Cookies.set('zones', JSON.stringify(cookieData) );
-
-
-  // }
 
 });
